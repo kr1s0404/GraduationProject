e3215b7be7d773ef7b6c59b94a0cc016513195a4
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-29 22:51:11 +0800

    Remove PredictedPoint

--- GraduationProject/Extensions/CGPoint+Extension.swift
+++ GraduationProject/Extensions/CGPoint+Extension.swift
@@ -28,6 +28,30 @@ extension CGPoint: Hashable {
 }
 
 extension CGPoint {
+    func angle(with p1: CGPoint, and p2: CGPoint) -> CGFloat {
+        let center = self
+        let transformedP1 = CGPoint(x: p1.x - center.x, y: p1.y - center.y)
+        let transformedP2 = CGPoint(x: p2.x - center.x, y: p2.y - center.y)
+        
+        let angleToP1 = atan2(transformedP1.y, transformedP1.x)
+        let angleToP2 = atan2(transformedP2.y, transformedP2.x)
+        
+        return normaliseToInteriorAngle(with: angleToP2 - angleToP1)
+    }
+    
+    func normaliseToInteriorAngle(with angle: CGFloat) -> CGFloat {
+        var angle = angle
+        if (angle < 0) { angle += (2*CGFloat.pi) }
+        if (angle > CGFloat.pi) { angle = 2*CGFloat.pi - angle }
+        return angle
+    }
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+extension CGPoint {
     /// Euclidean Distance
     static func distance(_ a: CGPoint, _ b: CGPoint) -> Double {
         return sqrt(Double((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)))
