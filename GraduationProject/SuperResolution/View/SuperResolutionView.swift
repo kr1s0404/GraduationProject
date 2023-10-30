30dde3c236e3a70450b797dee645066ead03efee
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 13:54:01 +0800

    Add SuperResolutionView

---
+++ GraduationProject/SuperResolution/View/SuperResolutionView.swift
@@ -0,0 +1,24 @@
+//
+//  SuperResolutionView.swift
+//  GraduationProject
+//
+//  Created by Kris on 10/30/23.
+//
+
+import SwiftUI
+
+struct SuperResolutionView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SuperResolutionView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        SuperResolutionView()
+    }
+}
