4a19f2a90edf46ad70ee0a8b7f21b3d31390b7f2
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 12:40:34 +0800

    Update MediaService Class

--- GraduationProject/Firebase/Service/MediaService.swift
+++ GraduationProject/Firebase/Service/MediaService.swift
@@ -1,5 +1,5 @@
 //
-//  ImageService.swift
+//  MediaService.swift
 //  GraduationProject
 //
 //  Created by Kris on 10/29/23.
@@ -8,7 +8,7 @@
 import SwiftUI
 import FirebaseStorage
 
-final class ImageService: ImageServiceProtocol
+final class MediaService: MediaServiceProtocol
 {
     private let storage = Storage.storage()
     
@@ -18,6 +18,15 @@ final class ImageService: ImageServiceProtocol
         metadata.contentType = "image/jpeg"
 
         let _ = try await storageRef.putDataAsync(imageData, metadata: metadata)
+        return try await storageRef.downloadURL()
+    }
+    
+    func uploadVideo(videoData: Data, in path: String) async throws -> URL {
+        let storageRef = storage.reference().child(path)
+        let metadata = StorageMetadata()
+        metadata.contentType = "video/mov"
+        
+        let _ = try await storageRef.putDataAsync(videoData, metadata: metadata)
         return try await storageRef.downloadURL()
     }
 }
