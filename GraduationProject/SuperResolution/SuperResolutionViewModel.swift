0eabeb8d6b8d7dfaef3e49d1861cd11a2b064af3
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 13:39:10 +0800

    Add SuperSolutionViewModel

---
+++ GraduationProject/SuperResolution/SuperResolutionViewModel.swift
@@ -0,0 +1,126 @@
+//
+//  SuperSolutionViewModel.swift
+//  GraduationProject
+//
+//  Created by Kris on 10/30/23.
+//
+
+import SwiftUI
+import CoreML
+import CoreImage
+
+final class SuperSolutionViewModel: ObservableObject
+{
+    @Published var model: SuperResolutionModel?
+    @Published var image: UIImage?
+    @Published var imageAfterSR: UIImage?
+    
+    private var originalImageSize: CGSize?
+    private var resizedImageSize: CGSize = CGSize(width: 512, height: 512)
+    
+    init() {
+        loadModel()
+    }
+    
+    private func loadModel() {
+        do {
+            let config = MLModelConfiguration()
+            self.model = try SuperResolutionModel(configuration: config)
+        } catch {
+            print("Error loading model: \(error)")
+        }
+    }
+    
+    func makeImageSuperResolution() {
+        guard let model = model, let image = image else {
+            print("Model or Image is nil")
+            return
+        }
+        self.originalImageSize = image.size
+        guard let resizedImage = image.resizeImageMaintainingAspectRatio(to: resizedImageSize) else {
+            print("Model can not be resize")
+            return
+        }
+        guard let imageToBuffer = resizedImage.convertToBuffer() else {
+            print("Failed to convert image to buffer")
+            return
+        }
+        do {
+            let prediction = try model.prediction(x_1: imageToBuffer)
+            let srImage = imageFromBuffer(prediction.activation_out)
+            self.imageAfterSR = srImage?.resizeImageMaintainingAspectRatio(to: self.originalImageSize ?? resizedImageSize)
+        } catch {
+            print("Error during image super-resolution: \(error)")
+        }
+    }
+    
+    func imageFromBuffer(_ pixelBuffer: CVPixelBuffer) -> UIImage? {
+        CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly)
+        defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly) }
+        
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let context = CIContext(options: nil)
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return nil }
+        
+        return UIImage(cgImage: cgImage)
+    }
+}
+
+extension UIImage {
+    func convertToBuffer() -> CVPixelBuffer? {
+        let attributes = [
+            kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+            kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
+        ] as CFDictionary
+        
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width),
+                                         Int(self.size.height), kCVPixelFormatType_32ARGB,
+                                         attributes, &pixelBuffer)
+        
+        guard status == kCVReturnSuccess, let pixelBuffer = pixelBuffer else {
+            print("Failed to create pixel buffer")
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0)) }
+        
+        guard let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer) else {
+            return nil
+        }
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData, width: Int(self.size.width),
+                                      height: Int(self.size.height), bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+                                      space: rgbColorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            return nil
+        }
+        
+        context.translateBy(x: 0, y: self.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        
+        return pixelBuffer
+    }
+    
+    func resizeImageMaintainingAspectRatio(to size: CGSize) -> UIImage? {
+        let aspectWidth = size.width / self.size.width
+        let aspectHeight = size.height / self.size.height
+        let aspectRatio = min(aspectWidth, aspectHeight)
+        
+        let newSize = CGSize(width: self.size.width * aspectRatio, height: self.size.height * aspectRatio)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        self.draw(in: CGRect(origin: .zero, size: newSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+}
+
+
