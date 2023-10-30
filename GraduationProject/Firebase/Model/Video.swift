20ca422e128aebc8def8fe0db25d087824a748f9
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 12:40:09 +0800

    Add VideoData

---
+++ GraduationProject/Firebase/Model/Video.swift
@@ -0,0 +1,14 @@
+//
+//  Video.swift
+//  GraduationProject
+//
+//  Created by Kris on 10/30/23.
+//
+
+import Foundation
+
+struct VideoData: Codable, Identifiable
+{
+    let id: String
+    var videoURL: String
+}
