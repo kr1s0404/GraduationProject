b97bac7b80c2e892663a254f2a2bbad7cbcfda79
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 17:36:56 +0800

    Add alert and loading view to SuperResolutionView

--- GraduationProject/SuperResolution/View/SuperResolutionView.swift
+++ GraduationProject/SuperResolution/View/SuperResolutionView.swift
@@ -25,42 +25,67 @@ struct SuperResolutionView: View
                         case .image(let image):
                             ScrollView
                             {
-                                Image(uiImage: image)
-                                    .resizable()
-                                    .scaledToFit()
-                                
-                                Divider()
-                                    .padding(.vertical)
-                                
-                                if let imageAfterSuperResolution = superResolutionVM.imageAfterSR {
-                                    Image(uiImage: imageAfterSuperResolution)
-                                        .resizable()
-                                        .scaledToFit()
-                                }
+                                mediaContentView(image: image)
                             }
-                            .toolbar {
-                                ToolbarItem(placement: .navigationBarLeading) {
-                                    Button("超解析度還原") { superResolutionVM.makeImageSuperResolution(from: image) }
-                                }
-                            }
+                            .toolbar { makeImageSuperResolutionButton(image: image) }
                         case .video( _):
                             Text("Can not select video")
                                 .foregroundColor(.red)
                     }
                 }
             }
+            .alert(superResolutionVM.errorMessage,
+                   isPresented: $superResolutionVM.showAlert,
+                   actions: { Text("OK") })
+            .overlay { if superResolutionVM.isLoading { loadingSpineer } }
             .sheet(isPresented: $showMediaPicker) { MediaPicker(media: $selectedMeida) }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("開啟相簿") { showMediaPicker.toggle() }
-                }
-            }
+            .toolbar { showMediaPickerButton }
         }
     }
 }
 
 extension SuperResolutionView {
+    @ToolbarContentBuilder
+    private func makeImageSuperResolutionButton(image: UIImage) -> some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button("超解析度還原") { superResolutionVM.makeImageSuperResolution(from: image) }
+        }
+    }
     
+    @ToolbarContentBuilder
+    private var showMediaPickerButton: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Button("開啟相簿") { showMediaPicker.toggle() }
+        }
+    }
+    
+    @ViewBuilder
+    private func mediaContentView(image: UIImage) -> some View {
+        VStack
+        {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+            
+            Divider()
+                .padding(.vertical)
+            
+            if let imageAfterSuperResolution = superResolutionVM.imageAfterSR {
+                Image(uiImage: imageAfterSuperResolution)
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var loadingSpineer: some View {
+        ProgressView("Converting Image...")
+            .frame(width: 250, height: 250)
+            .background(Material.ultraThinMaterial)
+            .cornerRadius(25)
+            .foregroundColor(.white)
+    }
 }
 
 struct SuperResolutionView_Previews: PreviewProvider
