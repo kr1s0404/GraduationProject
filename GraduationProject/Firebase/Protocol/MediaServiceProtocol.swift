eddd82c4e1a4baa6d6790f61ab0e4972dd93c188
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 12:40:43 +0800

    Update MediaServiceProtocol

--- GraduationProject/Firebase/Protocol/MediaServiceProtocol.swift
+++ GraduationProject/Firebase/Protocol/MediaServiceProtocol.swift
@@ -1,5 +1,5 @@
 //
-//  ImageServiceProtocol.swift
+//  MediaServiceProtocol.swift
 //  GraduationProject
 //
 //  Created by Kris on 10/29/23.
@@ -8,6 +8,7 @@
 import Foundation
 import FirebaseStorage
 
-protocol ImageServiceProtocol {
+protocol MediaServiceProtocol {
     func uploadImage(imageData: Data, in path: String) async throws -> URL
+    func uploadVideo(videoData: Data, in path: String) async throws -> URL
 }
