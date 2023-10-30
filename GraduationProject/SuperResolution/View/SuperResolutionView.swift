e677cc83a01d9613824b53ebd09294b8b71849fc
kr1s0404 <tylerlolz0404@gmail.com>
2023-10-30 15:10:09 +0800

    Update SuperResolutionView

--- GraduationProject/SuperResolution/View/SuperResolutionView.swift
+++ GraduationProject/SuperResolution/View/SuperResolutionView.swift
@@ -9,10 +9,58 @@ import SwiftUI
 
 struct SuperResolutionView: View
 {
+    @StateObject var superResolutionVM = SuperResolutionViewModel()
+    
+    @State var showMediaPicker: Bool = false
+    @State var selectedMeida: Media?
+    
     var body: some View
     {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack
+        {
+            VStack
+            {
+                if let media = selectedMeida {
+                    switch media {
+                        case .image(let image):
+                            ScrollView
+                            {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                Divider()
+                                    .padding(.vertical)
+                                
+                                if let imageAfterSuperResolution = superResolutionVM.imageAfterSR {
+                                    Image(uiImage: imageAfterSuperResolution)
+                                        .resizable()
+                                        .scaledToFit()
+                                }
+                            }
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                    Button("超解析度還原") { superResolutionVM.makeImageSuperResolution(from: image) }
+                                }
+                            }
+                        case .video( _):
+                            Text("Can not select video")
+                                .foregroundColor(.red)
+                    }
+                }
+            }
+            .sheet(isPresented: $showMediaPicker) { MediaPicker(media: $selectedMeida) }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("開啟相簿") { showMediaPicker.toggle() }
+                }
+            }
+        }
     }
+}
+
+extension SuperResolutionView {
+    
 }
 
 struct SuperResolutionView_Previews: PreviewProvider
