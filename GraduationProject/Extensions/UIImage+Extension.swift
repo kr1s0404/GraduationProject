fc45acbe9d909dae9e400b7c0de9a7f84a688249
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-11 04:14:15 +0800

    Add new function for process image before feed to the CoreML model

--- GraduationProject/Extensions/UIImage+Extension.swift
+++ GraduationProject/Extensions/UIImage+Extension.swift
@@ -6,18 +6,96 @@
 //
 
 import UIKit
+import CoreML
+import Accelerate
+import CoreGraphics
 
 extension UIImage {
-    func convertUIImageToCVPixelBuffer(_ image: UIImage) -> CVPixelBuffer? {
+    func getPixelData(buffer: inout [Double]) {
+        guard let cgImage = self.cgImage else { return }
+        
+        let size = self.size
+        let width = Int(size.width)
+        let height = Int(size.height)
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let bitsPerComponent = 8
+        
+        var pixelData = [UInt8](repeating: 0, count: width * height * bytesPerPixel)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: &pixelData,
+                                      width: width,
+                                      height: height,
+                                      bitsPerComponent: bitsPerComponent,
+                                      bytesPerRow: bytesPerRow,
+                                      space: colorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue) else { return }
+        
+        context.draw(cgImage, in: CGRect(origin: .zero, size: size))
+        
+        buffer.removeAll(keepingCapacity: true) // Ensure buffer is empty and has capacity
+        
+        // Using pointer to directly manipulate pixel data
+        pixelData.withUnsafeBufferPointer { ptr in
+            let totalPixels = width * height
+            buffer.reserveCapacity(totalPixels * 3) // Reserve space for RGB components
+            
+            for i in 0 ..< totalPixels {
+                let offset = i * bytesPerPixel
+                buffer.append(Double(ptr[offset]))     // Red
+                buffer.append(Double(ptr[offset + 1])) // Green
+                buffer.append(Double(ptr[offset + 2])) // Blue
+            }
+        }
+    }
+    
+    
+    func prewhiten(input: inout [Double], output: inout MLMultiArray) {
+        // Calculate mean using Accelerate
+        var mean = 0.0
+        vDSP_meanvD(input, 1, &mean, vDSP_Length(input.count))
+        
+        // Subtract mean from input values in place
+        var negativeMean = -mean
+        vDSP_vsaddD(input, 1, &negativeMean, &input, 1, vDSP_Length(input.count))
+        
+        // Calculate the sum of squares of input using Accelerate
+        var sumOfSquares = 0.0
+        vDSP_svesqD(input, 1, &sumOfSquares, vDSP_Length(input.count))
+        
+        // Calculate standard deviation
+        let stdDev = sqrt(sumOfSquares / Double(input.count))
+        
+        // Adjust the standard deviation with max(stdDev, 1/sqrt(inputCount))
+        let stdDevAdj = max(stdDev, 1.0 / sqrt(Double(input.count)))
+        
+        // Divide input by stdDevAdj and convert to MLMultiArray
+        let scaleFactor = 1.0 / stdDevAdj
+        var result = input // Use a local variable to store the result temporarily
+        
+        vDSP_vsmulD(input, 1, [scaleFactor], &result, 1, vDSP_Length(input.count))
+        
+        // Assign values to output MLMultiArray
+        for (index, value) in result.enumerated() {
+            output[index] = NSNumber(value: value)
+        }
+    }
+}
+
+extension UIImage {
+    func toCVPixelBuffer() -> CVPixelBuffer? {
         let attrs = [
             kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
         ] as CFDictionary
         var pixelBuffer: CVPixelBuffer?
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+        
         let status = CVPixelBufferCreate(kCFAllocatorDefault,
-                                         Int(image.size.width),
-                                         Int(image.size.height),
-                                         kCVPixelFormatType_32ARGB,
+                                         width,
+                                         height,
+                                         kCVPixelFormatType_32BGRA,
                                          attrs,
                                          &pixelBuffer)
         
@@ -25,27 +103,28 @@ extension UIImage {
             return nil
         }
         
-        CVPixelBufferLockBaseAddress(unwrappedPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        CVPixelBufferLockBaseAddress(unwrappedPixelBuffer, [])
         let pixelData = CVPixelBufferGetBaseAddress(unwrappedPixelBuffer)
         
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
         guard let context = CGContext(data: pixelData,
-                                      width: Int(image.size.width),
-                                      height: Int(image.size.height),
+                                      width: width,
+                                      height: height,
                                       bitsPerComponent: 8,
                                       bytesPerRow: CVPixelBufferGetBytesPerRow(unwrappedPixelBuffer),
                                       space: rgbColorSpace,
                                       bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            CVPixelBufferUnlockBaseAddress(unwrappedPixelBuffer, [])
             return nil
         }
         
-        context.translateBy(x: 0, y: image.size.height)
+        context.translateBy(x: 0, y: CGFloat(height))
         context.scaleBy(x: 1.0, y: -1.0)
         
         UIGraphicsPushContext(context)
-        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        self.draw(in: CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
         UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(unwrappedPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        CVPixelBufferUnlockBaseAddress(unwrappedPixelBuffer, [])
         
         return unwrappedPixelBuffer
     }
