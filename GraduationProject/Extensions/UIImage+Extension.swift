7082e00b922bfef015298907f5fcee98630fa542
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-21 02:57:10 +0800

    Update UIImage extension

--- GraduationProject/Extensions/UIImage+Extension.swift
+++ GraduationProject/Extensions/UIImage+Extension.swift
@@ -89,49 +89,48 @@ extension UIImage {
 
 extension UIImage {
     func toCVPixelBuffer() -> CVPixelBuffer? {
-        let attrs = [
+        let attributes = [
             kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
         ] as CFDictionary
+        
         var pixelBuffer: CVPixelBuffer?
-        let width = Int(self.size.width)
-        let height = Int(self.size.height)
         
-        let status = CVPixelBufferCreate(kCFAllocatorDefault,
-                                         width,
-                                         height,
-                                         kCVPixelFormatType_32ARGB,
-                                         attrs,
-                                         &pixelBuffer)
+        let status = CVPixelBufferCreate(
+            kCFAllocatorDefault, Int(self.size.width),
+            Int(self.size.height),
+            kCVPixelFormatType_32ARGB,
+            attributes,
+            &pixelBuffer)
         
-        guard status == kCVReturnSuccess, let unwrappedPixelBuffer = pixelBuffer else {
+        guard (status == kCVReturnSuccess) else {
             return nil
         }
         
-        CVPixelBufferLockBaseAddress(unwrappedPixelBuffer, [])
-        let pixelData = CVPixelBufferGetBaseAddress(unwrappedPixelBuffer)
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
         
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        guard let context = CGContext(data: pixelData,
-                                      width: width,
-                                      height: height,
-                                      bitsPerComponent: 8,
-                                      bytesPerRow: CVPixelBufferGetBytesPerRow(unwrappedPixelBuffer),
-                                      space: rgbColorSpace,
-                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
-            CVPixelBufferUnlockBaseAddress(unwrappedPixelBuffer, [])
-            return nil
-        }
         
-        context.translateBy(x: 0, y: CGFloat(height))
-        context.scaleBy(x: 1.0, y: -1.0)
+        let context = CGContext(
+            data: pixelData,
+            width: Int(self.size.width),
+            height: Int(self.size.height),
+            bitsPerComponent: 8,
+            bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
+            space: rgbColorSpace,
+            bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
         
-        UIGraphicsPushContext(context)
-        self.draw(in: CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height)))
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
         UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(unwrappedPixelBuffer, [])
         
-        return unwrappedPixelBuffer
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
     }
     
     func resized(to newSize: CGSize) -> UIImage? {
