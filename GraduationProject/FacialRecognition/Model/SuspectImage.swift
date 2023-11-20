3517a001a8f7070f5e965989e1a2ed6ca2ebbd73
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-20 16:44:40 +0800

    Add SuspectImage model

---
+++ GraduationProject/FacialRecognition/Model/SuspectImage.swift
@@ -0,0 +1,15 @@
+//
+//  SuspectImage.swift
+//  GraduationProject
+//
+//  Created by Kris on 11/11/23.
+//
+
+import SwiftUI
+
+struct SuspectImage: Identifiable
+{
+    let id = UUID()
+    let uiImage: UIImage
+    let imageURL: String
+}
