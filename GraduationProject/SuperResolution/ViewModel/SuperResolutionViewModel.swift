7ef8b22fdf9d2a44b1c1c737cee7b93556f5e21a
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-21 03:00:19 +0800

    Refactor SuperResolutionViewModel

--- GraduationProject/SuperResolution/ViewModel/SuperResolutionViewModel.swift
+++ GraduationProject/SuperResolution/ViewModel/SuperResolutionViewModel.swift
@@ -45,12 +45,12 @@ final class SuperResolutionViewModel: ObservableObject
         }
         
         let originalImageSize = image.size
-        guard let resizedImage = image.resizeImageToExactSize(to: resizedImageSize) else {
+        guard let resizedImage = image.resized(to: resizedImageSize) else {
             handleError(errorMessage: "Failed to resize image.")
             return
         }
         
-        guard let imageToBuffer = resizedImage.convertToBuffer() else {
+        guard let imageToBuffer = resizedImage.toCVPixelBuffer() else {
             handleError(errorMessage: "Failed to convert image to buffer.")
             return
         }
@@ -60,7 +60,7 @@ final class SuperResolutionViewModel: ObservableObject
             do {
                 let prediction = try model.prediction(x: imageToBuffer)
                 let srImage = self.imageFromBuffer(prediction.activation_out)
-                let processedImage = srImage?.resizeImageToExactSize(to: originalImageSize)
+                let processedImage = srImage?.resized(to: originalImageSize)
                 
                 // Update the UI on the main thread
                 DispatchQueue.main.async {
@@ -92,63 +92,5 @@ final class SuperResolutionViewModel: ObservableObject
         showAlert.toggle()
         self.errorMessage = errorMessage
         self.isLoading = false
-    }
-}
-
-extension UIImage {
-    func convertToBuffer() -> CVPixelBuffer? {
-        let attributes = [
-            kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
-            kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue
-        ] as CFDictionary
-        
-        var pixelBuffer: CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width),
-                                         Int(self.size.height), kCVPixelFormatType_32ARGB,
-                                         attributes, &pixelBuffer)
-        
-        guard status == kCVReturnSuccess, let pixelBuffer = pixelBuffer else {
-            print("Failed to create pixel buffer")
-            return nil
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-        defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0)) }
-        
-        guard let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer) else {
-            return nil
-        }
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        guard let context = CGContext(data: pixelData, width: Int(self.size.width),
-                                      height: Int(self.size.height), bitsPerComponent: 8,
-                                      bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
-                                      space: rgbColorSpace,
-                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
-            return nil
-        }
-        
-        context.translateBy(x: 0, y: self.size.height)
-        context.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context)
-        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        UIGraphicsPopContext()
-        
-        return pixelBuffer
-    }
-    
-    func resizeImageToExactSize(to size: CGSize) -> UIImage? {
-        let targetWidth = size.width
-        let targetHeight = size.height
-        
-        let rectangle = CGRect(x: 0, y: 0, width: targetWidth, height: targetHeight)
-        
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-        self.draw(in: rectangle)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage
     }
 }
