d6c14e408d6f1081427df715b804103069c1eede
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-23 11:57:40 +0800

    Handle Pinch Gesture on Camera

--- GraduationProject/UIKit/CameraUIViewController.swift
+++ GraduationProject/UIKit/CameraUIViewController.swift
@@ -8,17 +8,16 @@
 import UIKit
 import AVFoundation
 
-class CameraUIViewController: UIViewController
+class CameraUIViewController: UIViewController, UIGestureRecognizerDelegate
 {
     var captureSession: AVCaptureSession?
     var previewLayer: AVCaptureVideoPreviewLayer?
+    var captureDevice: AVCaptureDevice?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let captureSession = captureSession else {
-            return
-        }
+        guard let captureSession = captureSession else { return }
         
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer?.frame = view.layer.bounds
@@ -27,6 +26,11 @@ class CameraUIViewController: UIViewController
         if let previewLayer = previewLayer {
             view.layer.addSublayer(previewLayer)
         }
+        
+        let pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture(_:)))
+        pinchRecognizer.delegate = self
+        view.addGestureRecognizer(pinchRecognizer)
+        captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -41,6 +45,26 @@ class CameraUIViewController: UIViewController
         DispatchQueue.global(qos: .userInitiated).async {
             if !(self.captureSession?.isRunning ?? false) {
                 self.captureSession?.startRunning()
+            }
+        }
+    }
+    
+    @objc func handlePinchGesture(_ gesture: UIPinchGestureRecognizer) {
+        guard let device = captureDevice else { return }
+        
+        if gesture.state == .changed {
+            let maxZoomFactor = device.activeFormat.videoMaxZoomFactor
+            let pinchVelocityDividerFactor: CGFloat = 5.0
+            
+            do {
+                try device.lockForConfiguration()
+                defer { device.unlockForConfiguration() }
+                
+                var newZoomFactor = device.videoZoomFactor + atan2(gesture.velocity, pinchVelocityDividerFactor)
+                newZoomFactor = max(1.0, min(newZoomFactor, maxZoomFactor))
+                device.videoZoomFactor = newZoomFactor
+            } catch {
+                print("Error locking configuration")
             }
         }
     }
