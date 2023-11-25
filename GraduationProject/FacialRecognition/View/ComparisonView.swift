20901212aa4c6787e56b2feb465ebad23e18c18e
kr1s0404 <tylerlolz0404@gmail.com>
2023-11-26 07:20:51 +0800

    Update ComparisonView UI to show all the result

--- GraduationProject/FacialRecognition/View/ComparisonView.swift
+++ GraduationProject/FacialRecognition/View/ComparisonView.swift
@@ -2,7 +2,7 @@
 //  ComparisonView.swift
 //  GraduationProject
 //
-//  Created by Kris on 11/21/23.
+//  Created by Kris on 11/2/23.
 //
 
 import SwiftUI
@@ -11,50 +11,47 @@ struct ComparisonView: View
 {
     @ObservedObject var faceDetectionVM: FaceDetectionViewModel
     
-    @State var suspctImageAfterSR: UIImage?
-    @State var cuurentImageAfterSR: UIImage?
-    
     var body: some View
     {
-        VStack
+        NavigationStack
         {
-            if let suspectImage = faceDetectionVM.suspect?.uiImage,
-               let currentImage = faceDetectionVM.capturedImage {
-                HStack
-                {
-                    Image(uiImage: suspectImage)
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                HStack
-                {
-                    Image(uiImage: currentImage)
-                        .resizable()
-                        .scaledToFit()
+            VStack
+            {
+                List(faceDetectionVM.suspectList) { suspect in
+                    HStack
+                    {
+                        Image(uiImage: suspect.uiImage)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        if let detectedImage = suspect.detectedImage {
+                            Image(uiImage: detectedImage)
+                                .resizable()
+                                .scaledToFit()
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .overlay(alignment: .topTrailing) {
+                        if let suspectScore = suspect.score {
+                            scoreNumber(score: suspectScore)
+                        }
+                    }
                 }
             }
-            
-            Button {
-                faceDetectionVM.compareFacialFeature()
-            } label: {
-                Text("比對")
-            }
-            
-            Text("\(faceDetectionVM.possibilty, specifier: "%.2f")%")
-                .bold()
-                .foregroundColor(.white)
-                .frame(width: 130, height: 50)
-                .background(faceDetectionVM.possibilty > 80 ? .green : .red)
-                .cornerRadius(15)
+            .overlay { if faceDetectionVM.isLoading { ProgressView() } }
         }
     }
 }
 
-struct ComparisonView_Previews: PreviewProvider
-{
-    static var previews: some View
-    {
-        ComparisonView(faceDetectionVM: FaceDetectionViewModel())
+extension ComparisonView {
+    @ViewBuilder
+    private func scoreNumber(score: Double) -> some View {
+        Text("\(score * 100, specifier: "%.2f")")
+            .bold()
+            .foregroundColor(.white)
+            .frame(width: 60, height: 60)
+            .background(score > 0.7 ? .green : .red)
+            .cornerRadius(30)
     }
 }
